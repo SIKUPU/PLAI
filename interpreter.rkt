5d9f094af9d56cf5fde093803c51fc61be519754
SIKUPU <yoko.irony@gmail.com>
2020-03-02 11:52:16 +0900

    Update interpreter.rkt
    add parser(bottom half)

--- interpreter.rkt
+++ interpreter.rkt
@@ -9,3 +9,42 @@
   [numC (n : number)]
   [plusC (l : ArithC) (r : ArithC)]
   [multC (l : ArithC) (r : ArithC)])
+
+
+
+; -----------------------------
+;  Parser
+; -----------------------------
+
+; "(+ 23 (+ 5 6))"     Key Type
+;       |
+;     read
+;       |
+; '(+ 23 (+ 5 6))      S-expression
+;       |
+;     parse
+;       |
+; (plusC (numC 23)
+;        (plusC (numC 5)    ArithC
+;               (numC 6)))
+
+; S-expr -> ArithC
+; translates S-expression s to representation of Data
+(define (parse [s : s-expression]) : ArithC
+  (cond
+    [(s-exp-number? s) (numC (s-exp->number s))]
+    [(s-exp-list? s)
+     (let ([sl (s-exp->list s)])
+       (case (s-exp->symbol (first sl))
+         [(+) (plusC (parse (second sl)) (parse (third sl)))]
+         [(*) (multC (parse (second sl)) (parse (third sl)))]
+         [else (error 'parse "invalid list input")]))]
+    [else (error 'parse "invalid input")]))
+
+
+(test (parse '2) (numC 2))
+(test (parse '(+ 2 3)) (plusC (numC 2) (numC 3)))
+(test (parse '(+ 23 (+ 5 6)))
+      (plusC (numC 23)
+             (plusC (numC 5)
+                    (numC 6))))
