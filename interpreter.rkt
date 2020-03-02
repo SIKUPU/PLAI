d237c18fe9ea14af47c4ac7b57f180e5f6ac6f03
SIKUPU <yoko.irony@gmail.com>
2020-03-02 11:33:35 +0900

    Update interpreter.rkt
    add representation for Arithmetics

---
+++ interpreter.rkt
@@ -0,0 +1,11 @@
+#lang plai-typed
+
+
+; -----------------------------
+;  Data definition
+; -----------------------------
+
+(define-type ArithC
+  [numC (n : number)]
+  [plusC (l : ArithC) (r : ArithC)]
+  [multC (l : ArithC) (r : ArithC)])
